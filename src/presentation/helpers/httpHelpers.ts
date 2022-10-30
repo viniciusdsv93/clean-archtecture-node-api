25357ab99c69901a2bdd506ba3a9c92ffd0e6c94
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-10-30 14:33:31 -0300

    refactor: simplify code

--- src/presentation/helpers/httpHelpers.ts
+++ src/presentation/helpers/httpHelpers.ts
@@ -1,8 +1,7 @@
 import { HttpResponse } from '../protocols/http'
 
-export const badRequest = (error: Error): HttpResponse => {
-  return {
+export const badRequest = (error: Error): HttpResponse =>
+  ({
     statusCode: 400,
     body: error
-  }
-}
+  })
